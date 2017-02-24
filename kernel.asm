1c55695b94411e803c14fe63b3385d6d896cb71f
Ali Gholami <aligholami7596@gmail.com>
2017-02-24 04:13:50 -0500

    Compile kernel.asm with nasm

--- kernel.asm
+++ kernel.asm
@@ -1,9 +1,9 @@
-bits 32
+bits 	32
 section 	.text
 	align 	4
 	dd	0x1BADB002
 	dd	0x00
-	dd	- {0x1BADB002+0x00}
+	dd	- (0x1BADB002+0x00)
 
 global start
 extern cmain				;cmain function is in the kernel.c
