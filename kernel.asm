478d0c4c9c15f01ef64fbd84cf6d5c8b98bb8981
Ali Gholami <aligholami7596@gmail.com>
2017-02-24 03:18:29 -0500

    Add routine call to kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -4,3 +4,10 @@ section 	.text
 	dd	0x1BADB002
 	dd	0x00
 	dd	- {0x1BADB002+0x00}
+
+global start
+extern cmain				;cmain function is in the kernel.c
+start:
+	cli				;Clear the interrupts
+	call cmain			;Set program counter to fetch this routine
+	hlt				;Pause cpu from executing this routine
