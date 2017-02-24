eeaa9d4ee40b83e0202ab470affc0551daad3056
Ali Gholami <aligholami7596@gmail.com>
2017-02-24 08:15:03 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ nasm -f elf32 kernel.asm -o cinder.o
 gcc -m32 -c kernel.c -o cinderc.o
 ld -m elf_i386 -T link.ld -o kernel.bin cinder.o cinderc.o
 qemu-system-i386 -kernel kernel.bin
+grub-mkrescue -o CinderOS.iso CinderOS/
