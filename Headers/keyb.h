e2c5f2c0f2b60bbf4837ffcad7c029fd267617d8
Ali Gholami <aligholami7596@gmail.com>
2017-03-01 11:43:15 -0500

    Add 0-9 number recognition to the keyb.h

--- Headers/keyb.h
+++ Headers/keyb.h
@@ -23,8 +23,56 @@ string readStr()
 			/* We need to convert each code to it's ASCII representation */
 			switch(inportb(0x60))
 			{
-				case 1:
-
+			    case 2:
+			            printch('1');
+			            buffstr[i] = '1';
+			            i++;
+			            break;
+			    case 3:
+			            printch('2');
+			            buffstr[i] = '2';
+			            i++;
+			            break;
+			    case 4:
+			            printch('3');
+			            buffstr[i] = '3';
+			            i++;
+			            break;
+			    case 5:
+			            printch('4');
+			            buffstr[i] = '4';
+			            i++;
+			            break;
+			    case 6:
+			            printch('5');
+			            buffstr[i] = '5';
+			            i++;
+			            break;
+			    case 7:
+			            printch('6');
+			            buffstr[i] = '6';
+			            i++;
+			            break;
+			    case 8:
+			            printch('7');
+			            buffstr[i] = '7';
+			            i++;
+			            break;
+			    case 9:
+			            printch('8');
+			            buffstr[i] = '8';
+			            i++;
+			            break;
+			    case 10:
+			            printch('9');
+			            buffstr[i] = '9';
+			            i++;
+			            break;
+			    case 11:
+			            printch('0');
+			            buffstr[i] = '0';
+			            i++;
+			            break;
 
 			}
 		}
