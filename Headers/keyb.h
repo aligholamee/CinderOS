8ee119e606fc0ae0154ddec629a3c1b5e29d2159
Ali Gholami <aligholami7596@gmail.com>
2017-03-01 11:53:08 -0500

    Code cleanup

--- Headers/keyb.h
+++ Headers/keyb.h
@@ -128,6 +128,182 @@ string readStr()
 		                buffstr[i] = 'u';
 		                i++;
 		                break;
+		        case 23:
+		                printch('i');
+		                buffstr[i] = 'i';
+		                i++;
+		                break;
+		        case 24:
+		                printch('o');
+		                buffstr[i] = 'o';
+		                i++;
+		                break;
+		        case 25:
+		                printch('p');
+		                buffstr[i] = 'p';
+		                i++;
+		                break;
+		        case 26:
+		                printch('[');
+		                buffstr[i] = '[';
+		                i++;
+		                break;
+		        case 27:
+		                printch(']');
+		                buffstr[i] = ']';
+		                i++;
+		                break;
+		        case 28:
+		                printch('\n');
+		                buffstr[i] = '\n';
+		                i++;
+		                reading = 0;
+		                break;
+		        case 29:
+		                printch('q');           		/* Left Control */
+		                buffstr[i] = 'q';
+		                i++;
+		                break;
+		        case 30:
+		                printch('a');
+		                buffstr[i] = 'a';
+		                i++;
+		                break;
+		        case 31:
+		                printch('s');
+		                buffstr[i] = 's';
+		                i++;
+		                break;
+		        case 32:
+		                printch('d');
+		                buffstr[i] = 'd';
+		                i++;
+		                break;
+		        case 33:
+		                printch('f');
+		                buffstr[i] = 'f';
+		                i++;
+		                break;
+		        case 34:
+		                printch('g');
+		                buffstr[i] = 'g';
+		                i++;
+		                break;
+		        case 35:
+		                printch('h');
+		                buffstr[i] = 'h';
+		                i++;
+		                break;
+		        case 36:
+		                printch('j');
+		                buffstr[i] = 'j';
+		                i++;
+		                break;
+		        case 37:
+		                printch('k');
+		                buffstr[i] = 'k';
+		                i++;
+		                break;
+		        case 38:
+		                printch('l');
+		                buffstr[i] = 'l';
+		                i++;
+		                break;
+		        case 39:
+		                printch(';');
+		                buffstr[i] = ';';
+		                i++;
+		                break;
+		        case 40:
+		                printch((char)44);               /*   Single quote (') */
+		                buffstr[i] = (char)44;
+		                i++;
+		                break;
+		        case 41:
+		                printch((char)44);               /* Back tick (`) */
+		                buffstr[i] = (char)44;
+		                i++;
+		                break;
+		        case 42:                                 /* Left shift */
+		                printch('q');
+		                buffstr[i] = 'q';
+		                i++;
+		                break;
+		        case 43:                                 /* (< for somekeyboards)   */
+		                printch((char)92);
+		                buffstr[i] = 'q';
+		                i++;
+		                break;
+		        case 44:
+		                printch('z');
+		                buffstr[i] = 'z';
+		                i++;
+		                break;
+		        case 45:
+		                printch('x');
+		                buffstr[i] = 'x';
+		                i++;
+		                break;
+		        case 46:
+		                printch('c');
+		                buffstr[i] = 'c';
+		                i++;
+		                break;
+		        case 47:
+		                printch('v');
+		                buffstr[i] = 'v';
+		                i++;
+		                break;                
+		        case 48:
+		                printch('b');
+		                buffstr[i] = 'b';
+		                i++;
+		                break;               
+		        case 49:
+		                printch('n');
+		                buffstr[i] = 'n';
+		                i++;
+		                break;                
+		        case 50:
+		                printch('m');
+		                buffstr[i] = 'm';
+		                i++;
+		                break;               
+		        case 51:
+		                printch(',');
+		                buffstr[i] = ',';
+		                i++;
+		                break;                
+		        case 52:
+		                printch('.');
+		                buffstr[i] = '.';
+		                i++;
+		                break;            
+		        case 53:
+		                printch('/');
+		                buffstr[i] = '/';
+		                i++;
+		                break;            
+		        case 54:
+		                printch('.');
+		                buffstr[i] = '.';
+		                i++;
+		                break;            
+		        case 55:
+		                printch('/');
+		                buffstr[i] = '/';
+		                i++;
+		                break;            
+		        case 56:
+		                printch(' ');          			 /* Right shift */
+		                buffstr[i] = ' ';
+		                i++;
+		                break;           
+		        case 57:
+		                printch(' ');
+		                buffstr[i] = ' ';
+		                i++;
+		                break;
 
 			}
 		}
