139fbc87fae4ed1083f7d3d004dd2c4f1bdd029c
Ali Gholami <aligholami7596@gmail.com>
2017-03-01 13:40:42 -0500

    Move object files to the Objects folder

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 nasm -f elf32 kernel.asm -o cinder.o
 gcc -m32 -c kernel.c -o cinderc.o -ffreestanding
-ld -m elf_i386 -T link.ld -o kernel.bin cinder.o cinderc.o
-qemu-system-i386 -kernel kernel.bin
+gcc -m32 -c Headers/system.c -o Objects/system.o -ffreestanding
+gcc -m32 -c Headers/string.c -o Objects/string.o -ffreestanding  
+gcc -m32 -c Headers/screen.c -o Objects/screen.o -ffreestanding                                                     
+gcc -m32 -c Headers/keyb.c -o Objects/keyb.o -ffreestanding 
+ld -m elf_i386 -T link.ld -o CinderOS/boot/kernel.bin cinder.o cinderc.o Objects/system.o Objects/string.o Objects/screen.o Objects/keyb.o 
+qemu-system-i386 -kernel CinderOS/boot/kernel.bin
 grub-mkrescue -o CinderOS.iso CinderOS/
 
 read a
