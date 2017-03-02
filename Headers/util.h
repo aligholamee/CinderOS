677e57c51fd1548e5bbe0fa6d7cff93e58399e89
Ali Gholami <aligholami7596@gmail.com>
2017-03-02 13:53:13 -0500

    Add comments for clarification

--- Headers/util.h
+++ Headers/util.h
@@ -3,8 +3,13 @@
 
 #include "types.h"
 
+/* Copying data in the memory */
 void memory_copy(char *soruce, char *dest, int nbytes);
+
+/* Assigning a value to an specific address in the memory for len times */
 void memory_set(uint8 *dest, uint8 val, uint32 len);
+
+/* Converting integer to string */
 void int_to_ascii(int n, char str[]);
 
 #endif
