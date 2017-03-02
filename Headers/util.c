37411a7088935a009700d690085fdcfee021da28
Ali Gholami <aligholami7596@gmail.com>
2017-03-02 14:12:17 -0500

    Interrupts: Adding int_to_ascii convertor - NOT COMPLETED YET

--- Headers/util.c
+++ Headers/util.c
@@ -17,3 +17,24 @@ void memory_set(uint8 *dest, uint8 val, uint32 len)
 		*temp++ = val;
 }
 
+void int_to_ascii(int n, char str[])
+{
+	int i, sign;
+	if((sign = n) < 0) n = -n;
+
+	i = 0;
+	do 
+	{
+		/* Take out each digit and calculate it's ascii */
+		str[i++] = n % 10 + '0';
+	} while((n /= 10) > 0);
+
+	/* Add negative sign */
+	if (sign < 0) str[i++] = '-';
+
+	/* Add null character */
+	str[i] = '\0';
+
+	/* Reverse str */
+}
+
