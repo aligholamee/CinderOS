bda48f9419ef28726ff2f3e77e599ae9f58c1475
Ali Gholami <aligholami7596@gmail.com>
2017-03-04 01:35:49 -0500

    SOLVER ERROR: request for member ‘high_offset’ in something not a structure or union

--- Headers/idt.c
+++ Headers/idt.c
@@ -7,7 +7,7 @@ void set_idt_gate(int n, uint32 handler)
 	idt[n].sel = KERNEL_CS;
 	idt[n].always0 = 0;
 	idt[n].flags = 0x8E;
-	idt.high_offset = high_16(handler);
+	idt[n].high_offset = high_16(handler);
 }
 
 void set_idt()
