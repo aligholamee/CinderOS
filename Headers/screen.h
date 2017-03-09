6cdaba0d90816243ff5ba807cc396cc9464b706a
Ali Gholami <aligholami7596@gmail.com>
2017-03-09 13:37:25 -0500

    modify screen.h header file

--- Headers/screen.h
+++ Headers/screen.h
@@ -13,6 +13,10 @@ void scrollUp(uint8 lineNumber);
 void checkNewLine();
 void printChar(char c);
 void print(string ch);
+void set_screen_color_from_color_code(int color_code);
+void set_screen_color(int text_color,int bg_color);
+void print_colored(string ch,int text_color,int bg_color);
+
 
 #endif
 
