803250908dbdd604abef51733429e80ac93d1911
Ali Gholami <aligholami7596@gmail.com>
2017-03-09 13:58:43 -0500

    clean up kernel.c / start the shell

--- src/kernel.c
+++ src/kernel.c
@@ -3,36 +3,12 @@
 #include "../Headers/isr.h"
 #include "../Headers/idt.h"
 #include "../Headers/keyb.h"
+#include "../Headers/shell.h"
 
 int cmain()
 {
 	isr_install();
 	clearScreen();
 	print("Cinder is up...\nWhat can i do?");
-	while(1)
-	{
-		print("\nCINDER> ");
-
-		string ch = readStr();
-
-		if(strEql(ch,"cmd") || strEql(ch,"CMD"))
-		{
-			print("Cinder is up...\nWhat can i do?");
-		}
-
-		else if(strEql(ch,"clear") || strEql(ch,"CLEAR"))
-		{
-			clearScreen();
-		}
-
-		else
-		{
-			print("Wrong Command!\n");
-		}
-
-		print("\n");
-	}
-
-	return 0;
-			
+	start_shell(0);		
 }
